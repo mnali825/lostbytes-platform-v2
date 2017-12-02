cccc0386576d7274f08678dce705a20ed2351460
mir-aclewhip <mna271@nyu.edu>
2017-12-01 21:34:16 -0500

    design updates

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
 var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 
-var Item = new mongoose.Schema({
+var ItemType = new mongoose.Schema({
   name:String,
+  purchase_cost:Number
+});
+
+var Item = new mongoose.Schema({
+  type:ItemType,
   weight:Number,
   cost:Number
 });
@@ -12,8 +17,7 @@ var Session = new mongoose.Schema({
   items:[Item],
   weight:Number,
   cost:Number
-
-})
+});
 
 var User = new mongoose.Schema({
   username:String,
@@ -23,13 +27,10 @@ var User = new mongoose.Schema({
   sessions:[String]
 });
 
-
 // NOTE: we're using passport-local-mongoose as a plugin
 // our schema for user looks pretty thin... but that's because
 // the plugin inserts salt, password and username
 User.plugin(passportLocalMongoose);
 mongoose.model('User', User)
-// mongoose.model('Image', Image)
-
 
 mongoose.connect('mongodb://localhost/lostbytes-v2');
