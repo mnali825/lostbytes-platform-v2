b36a3992151a799b02cf654123b824e030871e7e
mir-aclewhip <mna271@nyu.edu>
2017-12-03 18:34:05 -0500

    fixed session route.. again.. and create route and template for logged in dashboard - wrote function to aggregate weight and cost by menu item

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,13 +5,34 @@ module.exports = function(router) {
 
   router.get('/dashboard', function(req,res) {
     if (req.user) {
+      //Get all sessions from id's stored in user obj
       Session.find({_id: {$in:req.user.sessions} }, function(err, sessions) {
         console.log(sessions);
         var items = [];
         sessions.forEach(function(sess) {
           Array.prototype.push.apply(items,sess.items);
         });
-        res.render('dashboard', {items:items});
+
+        // Compress and calculate aggregate of repeated menu items
+        var output = [];
+        items.forEach(function(item) {
+          var existing = output.filter(function(val, i) {
+            return val.type.name == item.type.name;
+          });
+          if (existing.length) {
+            var existingIndex = output.indexOf(existing[0]);
+            output[existingIndex].weight += item.weight;
+            output[existingIndex].cost += item.cost;
+          } else {
+            output.push(item);
+          }
+        });
+
+        // Get total cost & weight
+        var totalCost = items.reduce(function(a,b) {return a.cost+b.cost;});
+        var totalWeight = items.reduce((a,b)=>a.weight+b.weight);
+
+        res.render('dashboard', {items:output, totalCost:totalCost, totalWeight:totalWeight});
       });
     } else {
       res.redirect('/');
