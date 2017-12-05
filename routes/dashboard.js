3a6cc12de08c0ce341eea80f67a3c464e22b5ec8
mir-aclewhip <mna271@nyu.edu>
2017-12-04 21:12:34 -0500

    changed dashboard - don't calculate data if there are no sessions

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,44 +7,47 @@ module.exports = function(router) {
     if (req.user) {
       //Get all sessions from id's stored in user obj
       Session.find({_id: {$in:req.user.sessions} }, function(err, sessions) {
-        console.log(sessions);
-        var items = [];
-        sessions.forEach(function(sess) {
-          Array.prototype.push.apply(items,sess.items);
-        });
-
-        // Compress and calculate aggregate of repeated menu items
-        var output = [];
-        items.forEach(function(item) {
-          var existing = output.filter(function(val, i) {
-            return val.type.name == item.type.name;
+        if (sessions) {
+          var items = [];
+          sessions.forEach(function(sess) {
+            Array.prototype.push.apply(items,sess.items);
           });
-          if (existing.length) {
-            var existingIndex = output.indexOf(existing[0]);
-            output[existingIndex].weight += item.weight;
-            output[existingIndex].cost += item.cost;
-          } else {
-            output.push(item);
-          }
-        });
 
-        // Get total cost & weight
-        var totalCost = 0;
-        var totalWeight = 0;
-        var recentWeight = 0;
-        var recentCost = 0;
+          // Compress and calculate aggregate of repeated menu items
+          var output = [];
+          items.forEach(function(item) {
+            var existing = output.filter(function(val, i) {
+              return val.type.name == item.type.name;
+            });
+            if (existing.length) {
+              var existingIndex = output.indexOf(existing[0]);
+              output[existingIndex].weight += item.weight;
+              output[existingIndex].cost += item.cost;
+            } else {
+              output.push(item);
+            }
+          });
 
-        if (items) {
+          // Get total cost & weight
+          var totalCost = 0;
+          var totalWeight = 0;
+          var recentWeight = 0;
+          var recentCost = 0;
+
+          
           items.forEach(function(ele) {
             totalCost+=ele.cost;
             totalWeight+=ele.weight;
           });
           recentCost = items[0].cost;
           recentWeight = items[0].weight;
+          
+
+          res.render('dashboard', {items:output, totalCost:totalCost, totalWeight:totalWeight, recentWeight:recentWeight, recentCost:recentCost});              
+        } else {
+          res.render('dashboard');
         }
 
-        res.render('dashboard', {items:output, totalCost:totalCost, totalWeight:totalWeight, recentWeight:recentWeight, recentCost:recentCost});        
-        
       });
     } else {
       res.redirect('/');
