0e5a1030ba7ccf763800551667c11010cc718fa1
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-12-07 16:50:06 -0800

    Completes update and delete

--- routes/menu.js
+++ routes/menu.js
@@ -6,41 +6,43 @@ module.exports = function(router) {
     if (req.user) {
       MenuItem.find({}, function(err, menuItems) {
         // console.log(menuItems)
-        res.render('menu', {menuItems:menuItems.reverse()});
+        res.render('menu', { menuItems: menuItems.reverse() });
       });
     } else {
       res.redirect('/');
     }
   });
 
-  router.post('/api/create-menu-item', function(req,res) {
+  router.post('/api/create-menu-item', function(req, res) {
     new MenuItem({
-      name:req.body.name,
-      cost:Number(req.body.cost)
+      name: req.body.name,
+      cost: Number(req.body.cost)
     }).save(function(err, menuItem) {
       console.log(menuItem);
     });
   });
 
-  router.put('/api/create-menu-item/:itemId', function(req, res){
-    MenuItem
-      .findOne({_id : req.params.itemId}, function(err, item){
-        item.name = req.body.name;
-        item.cost = Number(req.body.cost);
-        item.save(function(err, item){
-          if (err) {
-            res.send(err)
-          }
-        })
-      })
-  })
+  router.put('/api/create-menu-item/:itemId', function(req, res) {
+    MenuItem.findOne({ _id: req.params.itemId }, function(err, item) {
+      req.body.name
+        ? (item.name = req.body.name)
+        : (item.cost = Number(req.body.cost));
 
-  router.delete('/api/create-menu-item/:itemId', function(req, res){
+      item.save(function(err, item) {
+        console.log('successfully updated menu item');
+        if (err) {
+          res.send(err);
+        }
+      });
+    });
+  });
+
+  router.delete('/api/create-menu-item/:itemId', function(req, res) {
     MenuItem.findByIdAndRemove(req.params.itemId, (err, item) => {
       let response = {
-        message: "Successfully deleted menu item"
+        message: 'Successfully deleted menu item'
       };
-      res.status(200).send(response)
-    })
-  })
-}
+      res.status(200).send(response);
+    });
+  });
+};
