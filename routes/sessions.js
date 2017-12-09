83c0b38ee2dc6bb922ad4bf6104efe8bf3976e98
mir-aclewhip <mna271@nyu.edu>
2017-12-09 17:24:36 -0500

    i think we finished the core functionality

--- routes/sessions.js
+++ routes/sessions.js
@@ -22,41 +22,60 @@ module.exports = function(router) {
     }
   });
 
-  router.post('/api/add-item/s=:sid&t=:tid', function(req,res) {
-    if (req.params.sid == 'guest') {
+  // router.post('/api/add-item/s=:sid&t=:tid', function(req,res) {
+  //   if (req.params.sid == 'guest') {
 
-    } else {
-      MenuItem.findOne({_id:req.params.tid}, function(err, menuItem) {
-        console.log('this is the menuitem we are adding: '+ menuItem);
-        var item = new Item({
-          type:menuItem,
-          weight:Number(req.body.weight),
-          cost:Number(req.body.weight)*Number(menuItem.cost)
-        }).save(function(err, item) {
-          console.log(err);
-          console.log('here: '+item);
-          Session.findOne({_id:req.params.sid}, function(err, sess) {
-            console.log('found the sesh: '+sess);
-            console.log(item);
+  //   } else {
+  //     MenuItem.findOne({_id:req.params.tid}, function(err, menuItem) {
+  //       console.log('this is the menuitem we are adding: '+ menuItem);
+  //       var item = new Item({
+  //         type:menuItem,
+  //         weight:Number(req.body.weight),
+  //         cost:Number(req.body.weight)*Number(menuItem.cost)
+  //       }).save(function(err, item) {
+  //         console.log(err);
+  //         console.log('here: '+item);
+  //         Session.findOne({_id:req.params.sid}, function(err, sess) {
+  //           console.log('found the sesh: '+sess);
+  //           console.log(item);
+  //           sess.items.push(item);
+  //           sess.weight += item.weight;
+  //           sess.cost += item.cost;
+  //           sess.save(function(err, sess) {
+  //             console.log(sess);
+  //             res.status(200).send(req.body);
+  //           });
+  //         });
+  //       });
+  //     });
+  //   }
+  // });
+
+  router.post('/api/complete-session', function(req,res) {
+    Session.findOne({_id:req.body.id}, function(err, sess) {
+      // Add all items to sess, then on save, add sess id to user
+      var items = JSON.parse(req.body.data);
+      items.forEach(function(ele) {
+        MenuItem.findOne({'_id':ele.tid}, function(err, menuItem) {
+          new Item({
+            type:menuItem,
+            weight:ele.weight,
+            cost:ele.cost
+          }).save(function(err, item) {
             sess.items.push(item);
             sess.weight += item.weight;
             sess.cost += item.cost;
-            sess.save(function(err, sess) {
+            sess.save(function(err,sess) {
               console.log(sess);
-              res.status(200).send(req.body);
             });
           });
         });
       });
-    }
-  });
-
-  router.post('/api/complete-session', function(req,res) {
-    Session.findOne({_id:req.body.id}, function(err, sess) {
       User.findOne({username:req.user.username}, function(err, user) {
         user.sessions.push(sess._id);
         user.save(function(err, user) {
           console.log(user);
+          res.redirect('/dashboard');
         });
       });
     });
@@ -65,6 +84,7 @@ module.exports = function(router) {
   router.post('/api/cancel-session', function(req,res) {
     Session.findOne({_id:req.body.id}).remove(function(err,sess) {
       console.log('removed: '+sess);
+      res.redirect('/dashboard');
     });
   });
 }
